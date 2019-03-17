bc213a7b4e9799b8ccd31f65edf61d56cf70c4d4
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-03-17 06:38:05 +0100

    Switches to react hooks (patch)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
 import shout from 'random-shout'
 import emojis from '../lib/emojis'
@@ -12,79 +12,66 @@ function getWord () {
   return shout(sentence)
 }
 
-const Index = class extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      word: getWord()
-    }
-    this.triggerDarkness = this.triggerDarkness.bind(this)
-  }
+const Index = () => {
+  const [word, setWord] = useState(getWord())
 
-  triggerDarkness () {
-    const word = getWord()
-    this.setState({ word: word })
-  }
-
-  render () {
-    return (
-      <div>
-        <Head>
-          <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-          <title>Hanna hater ting!</title>
-        </Head>
-        <div className={'wrapper'}>
-          <h1>Hanna hater ting!</h1>
-          <img src={'/static/hanna.jpg'} />
-          <div className='fortune-box'>
-            {this.state.word}
-          </div>
-          <p>
-            <button onClick={this.triggerDarkness}>Keep me going</button>
-          </p>
-          <style jsx global>
-            {`
-            body {
-              background-color: Black;
-              padding: 20px;
-              color: lime;
-            }
-            img {
-              border-radius: 25px;
-            }
-            button {
-              font-weight: 500;
-              width: 90%;
-              height: 60px;
-              font-size: 30px;
-              border-radius: 25px;
-              border: 1px solid black;
-              cursor: pointer;
-              display: inline-block;
-              text-decoration: none;
-              background-color: thistle;
-              outline: 0;
-            }
-            button:focus {
-              outline:0;
-            }
-            button:active {
-              outline: 0;
-              background-color: plum;
-            }
-            .fortune-box {
-              font-size: 48px;
-              padding: 15px;
-            }
-            .wrapper {
-              text-align: center;
-            }
-          `}
-          </style>
+  return (
+    <div>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <title>Hanna hater ting!</title>
+      </Head>
+      <div className={'wrapper'}>
+        <h1>Hanna hater ting!</h1>
+        <img src={'/static/hanna.jpg'} />
+        <div className='fortune-box'>
+          {word}
         </div>
+        <p>
+          <button onClick={() => setWord(getWord())}>Keep me going</button>
+        </p>
+        <style jsx global>
+          {`
+          body {
+            background-color: Black;
+            padding: 20px;
+            color: lime;
+          }
+          img {
+            border-radius: 25px;
+          }
+          button {
+            font-weight: 500;
+            width: 90%;
+            height: 60px;
+            font-size: 30px;
+            border-radius: 25px;
+            border: 1px solid black;
+            cursor: pointer;
+            display: inline-block;
+            text-decoration: none;
+            background-color: thistle;
+            outline: 0;
+          }
+          button:focus {
+            outline:0;
+          }
+          button:active {
+            outline: 0;
+            background-color: plum;
+          }
+          .fortune-box {
+            font-size: 48px;
+            padding: 15px;
+          }
+          .wrapper {
+            text-align: center;
+          }
+        `}
+        </style>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Index
